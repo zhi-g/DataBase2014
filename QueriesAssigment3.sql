eb09d55c7508093da25d3ad279239fe1f6626aa3
Merok <john.gaspoz@bluewin.ch>
2014-05-31 16:00:15 +0200

    query K

--- QueriesAssigment3.sql
+++ QueriesAssigment3.sql
@@ -1,3 +1,4 @@
+-- QUERY H not correct I think
 select distinct t2.name from
 (select distinct area.id
 from area, (select artist.areaid, count(distinct artist.name) as cnt
@@ -25,3 +26,21 @@ where cnt > 30) t1,
   order by countRecordedTracks desc
 ), artist r where r.id = artistid and r.type = 'Group' and r.areaid is not null)) t2
 where t1.id = t2.areaid;
+
+-- QUERY K
+--List all genres that have no female artists, all genres that have no male artists and all genres that have no groups. 
+
+-- no female
+select g.name 
+from genre G , artist A, artist_genre AG
+where a.id = ag.artistid and ag.genreid = g.id and a.gender != 'Female';
+
+-- no male
+select g.name 
+from genre G , artist A, artist_genre AG
+where a.id = ag.artistid and ag.genreid = g.id and a.gender != 'Male';
+
+-- no Group
+select g.name 
+from genre G , artist A, artist_genre AG
+where a.id = ag.artistid and ag.genreid = g.id and a.type != 'Group';
